4900eeac2fe4681d1405831c651c22ec8c668c19
Lane Spangler <las4vc@virginia.edu>
2018-04-03 21:52:25 -0400

    Get required names from heavy source and rename base module code based on the name of the heavy patch

--- heavygodot/commands/wrap.py
+++ heavygodot/commands/wrap.py
@@ -19,7 +19,7 @@ class Wrap(Base):
         self.initialize()
         self.extract_heavy_source()
         self.copy_base_module()
-        self.analyze_source()
+        self.analyze_heavy_source()
         self.transform_base_module()
         self.finish()
 
@@ -69,9 +69,6 @@ class Wrap(Base):
 
         zip_ref.extractall(self.module_dir)
         zip_ref.close()
-
-        if self.options['--clean']:
-            os.remove(self.zipfile_path)
     
     """ Copy all of the base module code into the new module directory """
     def copy_base_module(self):
@@ -82,12 +79,66 @@ class Wrap(Base):
                 shutil.copy(file_path, self.module_dir)
     
     """ Analyze the heavy source code to find the values for all the variables we need to resolve in the base module code """
-    def analyze_source(self):
+    def analyze_heavy_source(self):
+        self.get_names_from_source()
+        self.generate_event_methods()
+        self.generate_parameter_control_methods()
+        self.generate_documentation()
+
+    """ Get the heavy patch name from the source and use it to determine the module name, class names, and related symbols """
+    def get_names_from_source(self):
+        source_files = os.listdir(self.module_dir)
+        patch_name_regex = re.compile("(Heavy_.*?)\.(cpp|hpp)")
+        patch_name = None
+
+        for file_name in source_files:
+            match = patch_name_regex.match(file_name)
+            if match is not None:
+                patch_name = match.group(1)
+        
+        if patch_name is None:
+            print("Cannot find heavy patch in supplied source code")
+            cleanup_temp()
+            return
+        
+        self.variables['module_name'] = patch_name.lower()+"_module"
+        self.variables['patch_classname'] = patch_name
+        self.variables['audio_stream_classname'] = patch_name+"_AudioStream"
+        self.variables['audio_playback_classname'] = patch_name+"_AudioStreamPlayback"
+        self.variables['header_symbol'] = self.variables['module_name'].upper()+"_H"
+
+    """ Generate code to pass events between the heavy patch and the Godot AudioStream """
+    def generate_event_methods(self):
         pass
 
-    """ Transform the base module code into a wrapper for the supplied heavy source code """
-    def transform_base_module(self):
+    """ Generate code to expose heavy patch parameters through the Godot AudioStream """
+    def generate_parameter_control_methods(self):
         pass
+
+    """ Generate documentation for the new Godot module """
+    def generate_documentation(self):
+        pass
+
+    """ Transform the base module code into a wrapper for the supplied heavy source code by resolving all variables in the base module and renaming base files"""
+    def transform_base_module(self):
+        self.rename_files()
+
+    def rename_files(self):
+        new_module_dir = os.path.join(self.module_dir, os.path.join(os.pardir, self.variables['module_name']))
+
+        os.rename(self.module_dir, new_module_dir)
+        self.module_dir = os.path.abspath(new_module_dir)
+
+        base_src_regex = re.compile("(base).(cpp|h)")
+
+        source_files = os.listdir(self.module_dir)
+        for file_name in source_files:
+            match = base_src_regex.match(file_name)
+            if match is not None:
+                file_path = os.path.join(self.module_dir, file_name)
+                new_file_name = file_name[:match.start(1)] + self.variables['module_name'] + file_name[match.end(1):]
+                new_file_path = os.path.join (self.module_dir, new_file_name)
+                os.rename(file_path, new_file_path)
 
     """ Replace all of the expressions of the form $(var) with the value of var stored in the variables dictionary """
     def resolve_variables(self, input_file):
@@ -107,9 +158,16 @@ class Wrap(Base):
         
         return resolved
 
-    """ Clean up and exit"""
+    """ Cleanup and finish """
     def finish(self):
+        if self.options['--clean']:
+            os.remove(self.zipfile_path)
         print("Module generation complete. The heavy patch '"+"x"+"' has been wrapped up as the Godot module '"+"y"+"'")
+
+    """ Remove the temporary folder if the wrap failed prematurely """
+    def cleanup_temp(self):
+        shutil.rmtree(self.module_dir)
+
 
 
     
